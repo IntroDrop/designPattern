472f35549bc5d29bf9368f8d1606281c69a6c0d8
IntroDrop <skyhigh0u@gmail.com>
2022-02-26 17:28:08 +0800

    单例模式测试用例

--- singleton/singleton_test.go
+++ singleton/singleton_test.go
@@ -5,20 +5,23 @@ import (
 	"testing"
 )
 
-func Test(t *testing.T) {
-	t.Run("singleton: ", GetSingleton)
-}
-
-func GetSingleton(t *testing.T) {
+func TestGetSingleton(t *testing.T) {
 	/* 执行结果：
 	   执行获取单例对象
 	   instance1 = 0xc0000584a0, name=标题1
 	   instance2 = 0xc0000584a0, name=标题1
 	*/
 
+	// 第一次调用，instance里是"标题1"
 	instance1 := GetInstance("标题1")
 	fmt.Println(fmt.Sprintf("instance1 = %p, name=%s", instance1, instance1.Title))
 
-	instance2 := GetInstance("标题2")
-	fmt.Println(fmt.Sprintf("instance2 = %p, name=%s", instance2, instance2.Title))
+	// 之后调用，instance并没有重新被创建，拿到的还是原来的instance，取出的title还是"标题1"
+	for i := 0; i < 10; i++ {
+		go func() {
+			instance2 := GetInstance(fmt.Sprintf("标题%d", i))
+			fmt.Println(fmt.Sprintf("instance2 = %p, name=%s", instance2, instance2.Title))
+		}()
+	}
+
 }
